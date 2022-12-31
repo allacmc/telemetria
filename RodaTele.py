62aaa2809fbccb441e2457978e7575cc222b0edd
allacmc <allan@allancaldas.com.br>
2022-12-30 21:38:34 -0300

    Mais modificações do codigo do stevie

--- RodaTele.py
+++ RodaTele.py
@@ -18,11 +18,11 @@ topic2 = "belov/barco01/tele01/profundidade"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 username = 'CasaPendotiba'
 password = 'Casa12345678@#'
-"""
+
 if not sensor.init():
         print("Sensor could not be initialized")
         exit(1)
-"""
+
 def on_disconnect(client, userdata, rc):
    print("client disconnected ok")
    client.connected_flag=False
@@ -44,15 +44,13 @@ def publish(client):
     print("in publish")
     while True:
         time.sleep(1)
-        """
-        if sensor.read():
-            p = sensor.depth()
-            t = sensor.temperature()
-        else:
-            exit(1)
-        """
-        #t=20
-        #p=80
+        
+    if sensor.read():
+       p = sensor.depth()
+       t = sensor.temperature()
+    else:
+       exit(1)
+        
         if client.connected_flag: 
             result = client.publish(topic1, t)
             result2 = client.publish(topic2, p)
