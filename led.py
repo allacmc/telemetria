f22f3650e40b7b26a55dc22c546acf00d798167f
allacmc <allan@allancaldas.com.br>
2023-02-07 15:18:23 -0300

    Update led.py

--- led.py
+++ led.py
@@ -5,3 +5,11 @@ pinled = 8
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pinled, GPIO.OUT, initial=GPIO.LOW) 
+
+def status(status):
+    if status == True:
+       #GPIO.output(pinled, GPIO.HIGH) # Turn on
+       print("HIGH")
+    else:
+       #GPIO.output(pinled, GPIO.LOW) # Turn off
+       print("low")
