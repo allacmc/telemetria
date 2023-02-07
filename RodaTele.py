a0c8a2b863750c6f3f8183df97eb14ef0e9cc1e2
allacmc <allan@allancaldas.com.br>
2023-02-07 15:16:03 -0300

    Update RodaTele.py

--- RodaTele.py
+++ RodaTele.py
@@ -17,7 +17,6 @@ broker = '192.168.1.210'
 port = 1883
 topic1 = "belov/barco01/tele01/temperatura"
 topic2 = "belov/barco01/tele01/profundidade"
-pinled = 8
 tempoEnvio = 500/1000
 
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
