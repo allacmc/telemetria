9a28e5486a2bb4bed81352b3112b0e3ed0944e6d
allacmc <allan@allancaldas.com.br>
2023-02-07 18:55:12 -0300

    Verifique os detalhes abaixo
    Retirei a conferencia do resultado da publicação - porque ele não funciona. É indiferente, tirei o cabo de rede várias vezes do servidor e ele continua dizendo que está enviando com status 0.

--- RodaTele.py
+++ RodaTele.py
@@ -59,13 +59,8 @@ def publish(client):
            try: 
                result = client.publish(topic1, t)
                result2 = client.publish(topic2, p)
-               status = result[0]
-               if status == 0:
-                  print(f"Enviando dados para o Broker...", t, p)
-                  led.status(True)  
-                  print(f"Contador:", msg_count)
-               else:
-                  print(f"Failed to send message to topic {topic}")
+               print(f"Contador:", msg_count)     
+               led.status(True)       
            except:
                print("Falha na conexão..")
         msg_count += 1
