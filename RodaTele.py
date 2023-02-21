9ee3bd8363c38ddd967f076321f61eb1f9450cdd
root <allan@allancaldas.com.br>
2023-02-21 01:09:59 +0000

    Arquivo Executavel Linux

--- RodaTele.py
+++ RodaTele.py
@@ -1,4 +1,4 @@
-# python 3.6
+#!/usr/bin/env python3
 import sys
 sys.path.insert(1, '/ms5837-python')
 
--- test/piscaLed.py
+++ test/piscaLed.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
 from time import sleep # Import the sleep function from the time module
+
 GPIO.setwarnings(False) # Ignore warning for now
+
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
 pin_led = 16
@@ -9,7 +12,8 @@ GPIO.setup(pin_led, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin 
 
 
 while True: # Run forever
- GPIO.output(pin_led, GPIO.HIGH) # Turn on
- sleep(1) # Sleep for 1 second
- GPIO.output(pin_led, GPIO.LOW) # Turn off
- sleep(1) # Sleep for 1 second
+    sleep(1)
+    GPIO.output(pin_led, GPIO.HIGH) # Turn on
+    sleep(1) # Sleep for 1 second
+    GPIO.output(pin_led, GPIO.LOW) # Turn off
+    #sleep(0.5) # Sleep for 1 second
